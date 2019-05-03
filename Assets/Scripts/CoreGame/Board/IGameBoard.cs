ecba72f57424cf77a4c2ce8b98b184026eaa0b51
Alex Smirnov <97alexsmirnov@gmail.com>
2019-05-03 04:53:27 -0700

    Properly converted Interface classes into interfaces.

--- Assets/Scripts/CoreGame/Board/IGameBoard.cs
+++ Assets/Scripts/CoreGame/Board/IGameBoard.cs
@@ -1,21 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace CoreGame.Board
 {
-    public class IGameBoard : MonoBehaviour
+    public interface IGameBoard
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        
     }
 }
--- Assets/Scripts/UserInterface/Interfaces/IGameInfoDisplay.cs
+++ Assets/Scripts/UserInterface/Interfaces/IGameInfoDisplay.cs
@@ -1,17 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IGameInfoDisplay : MonoBehaviour
+namespace UserInterface.Interfaces
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public interface IGameInfoDisplay
     {
         
     }
