c1d1c9c58bf71ab1478d8e223842674385c1ddd4
Alex Smirnov <97alexsmirnov@gmail.com>
2019-05-03 22:59:43 -0700

    Fixed one function and added comments to GameBoard and LOGameBoard.

--- Assets/Scripts/CoreGame/Board/Interfaces/GameBoard.cs
+++ Assets/Scripts/CoreGame/Board/Interfaces/GameBoard.cs
@@ -7,12 +7,34 @@ namespace CoreGame.Board.Interfaces
     /// </summary>
     public abstract class GameBoard<TGameState, TMoveDataType> where TGameState : IGameState
     {
+        /// <summary>
+        /// Returns a boolean value indicating whether or not the game state marks the end of the game.
+        /// </summary>
+        /// <param name="gameState">The game state to assess.</param>
+        /// <returns><c>true</c> if the state marks the game's end, <c>false</c> otherwise.</returns>
         public abstract bool IsWon(TGameState gameState);
 
+        /// <summary>
+        /// Returns a new <see cref="TGameState"/> representing the state of the game after the given <paramref name="move"/> is performed.
+        /// </summary>
+        /// <param name="gameState">A <see cref="TGameState"/> representing the state of the game prior to making the move.</param>
+        /// <param name="move">A <see cref="TMoveDataType"/> representing a valid move for the given <see cref="GameBoard{TGameState,TMoveDataType}"/>.</param>
+        /// <returns>A new <see cref="TGameState"/> representing the state of the game after the move is made and <c>null</c> if the move was invalid.</returns>
         public abstract TGameState MakeMove(TGameState gameState, TMoveDataType move);
 
-        public abstract List<TMoveDataType> GetValidMoves(TGameState gameState, TMoveDataType move);
+        /// <summary>
+        /// Gets a list of valid moves to perform on the provided <paramref name="gameState"/>.
+        /// </summary>
+        /// <param name="gameState">The state of the game from which to determine available moves.</param>
+        /// <returns>A <see cref="List{T}"/> of the valid moves for the given state. Note that the amount of returned moves may be 0.</returns>
+        public abstract List<TMoveDataType> GetValidMoves(TGameState gameState);
 
+        /// <summary>
+        /// Checks if the given move is a valid one for the state.
+        /// </summary>
+        /// <param name="gameState">The <see cref="TGameState"/> against which to compare the provided <paramref name="move"/>.</param>
+        /// <param name="move">The move to check validity for.</param>
+        /// <returns><c>true</c> if the move is valid for the given board, <c>false</c> otherwise.</returns>
         public abstract bool IsValidMove(TGameState gameState, TMoveDataType move);
     }
 }
--- Assets/Scripts/CoreGame/Board/LOGameBoard.cs
+++ Assets/Scripts/CoreGame/Board/LOGameBoard.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using CoreGame.Board.Interfaces;
-using UnityEngine;
 using Utility;
 
 namespace CoreGame.Board
@@ -21,8 +20,7 @@ namespace CoreGame.Board
         {
             if (!IsValidMove(gameState, move)) return null;
             bool[] boardState = gameState.boardState;
-            List<int> adjacentSpaces = GetAdjacentSpaces(gameState, move);
-            adjacentSpaces.Add(move); // add the move itself to the adjacent spaces
+            List<int> adjacentSpaces = GetAffectedSpaces(gameState, move); // return value includes the source move
             foreach (int space in adjacentSpaces) // flip the state of any affected spaces
             {
                 boardState[space] = !boardState[space];
@@ -31,7 +29,7 @@ namespace CoreGame.Board
             return new LOGameState(gameState.dimensions, boardState, CheckBoardstateWin(boardState), adjacentSpaces);
         }
 
-        public override List<int> GetValidMoves(LOGameState gameState, int move)
+        public override List<int> GetValidMoves(LOGameState gameState)
         {
             return Enumerable.Range(0, gameState.boardState.Length).ToList();
         }
@@ -41,16 +39,22 @@ namespace CoreGame.Board
             return move >= 0 && move < gameState.boardState.Length;
         }
 
-        private List<int> GetAdjacentSpaces(LOGameState gameState, int spaceIndex)
+        /// <summary>
+        /// Returns a list of all spaces affected by the move. For this game, these are all spaces adjacent to <paramref name="spaceIndex"/>.
+        /// </summary>
+        /// <param name="gameState">The state used to determine which spaces are adjacent to <paramref name="spaceIndex"/>.</param>
+        /// <param name="spaceIndex">The array index representing the move made.</param>
+        /// <returns>A list of integers, including <paramref name="spaceIndex"/> that a move of the value <paramref name="spaceIndex"/> has affected.</returns>
+        private List<int> GetAffectedSpaces(LOGameState gameState, int spaceIndex) // possible to adjust this function to return spaces 1-over or the entire grid of spaces if desired for a different implementation
         {
-            List<int> adjacentSpaces = new List<int>();
+            List<int> adjacentSpaces = new List<int>{spaceIndex}; // add the source move to the list
             if (gameState.dimensions.Length < 1) return adjacentSpaces;
 
             int[] checkedDimensions = GetMultiplesArrayHelper(gameState.dimensions);
 
             for (int i = 0; i < checkedDimensions.Length-1; ++i)
             {
-                int spaceIndexRegion = spaceIndex/checkedDimensions[i+1]; //Important! Integer division!
+                int spaceIndexRegion = spaceIndex/checkedDimensions[i+1]; // important! integer division!
                 int prevSpace = spaceIndex - checkedDimensions[i];
                 int nextSpace = spaceIndex + checkedDimensions[i];
                 if (prevSpace / checkedDimensions[i + 1] == spaceIndexRegion)
@@ -67,6 +71,11 @@ namespace CoreGame.Board
             return adjacentSpaces;
         }
 
+        /// <summary>
+        /// A helper function that returns element-wise multiples of dimensions (with 1 prepended) for use in array indexing.
+        /// </summary>
+        /// <param name="dimensions">A <see cref="Array"/> of <see cref="int"/> representing the dimensions of the board.</param>
+        /// <returns>A <see cref="List{T}"/> of size <paramref name="dimensions"/>+1 with each element being the element-wise multiplication of all previous elements and itself.</returns>
         private int[] GetMultiplesArrayHelper(int[] dimensions)
         {
             int[] ret = new int[dimensions.Length+1];
@@ -79,6 +88,11 @@ namespace CoreGame.Board
             return ret;
         }
 
+        /// <summary>
+        /// Internal function to check wins. The interface implementation takes a shortcut and stores this value in state.
+        /// </summary>
+        /// <param name="boardState">A boolean array representing the state of the board.</param>
+        /// <returns><c>true</c> if all values in <paramref name="boardState"/> are false, <c>false</c> otherwise.</returns>
         private bool CheckBoardstateWin(bool[] boardState)
         {
             return boardState.All(space => space == false); // more explicit notation as opposed to !space
